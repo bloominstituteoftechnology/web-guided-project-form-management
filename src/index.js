e84c908cdeb2d19971bfe9051b439ca77c9e0ea6
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-05-18 12:33:26 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,7 +11,19 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList)
+  return (
+    <div className='container'>
+      <h1>Simple Form App</h1>
+      {pets.map((pet, idx) => {
+        return (
+          <div key={idx}>
+            {pet.petName} is a {pet.petType}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 render(
