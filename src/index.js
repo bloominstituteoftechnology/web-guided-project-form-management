ca9c3d36d098ddd322917b6d53154dd8440f5323
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-05-18 13:19:12 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -10,8 +10,23 @@ const petsList = [
   { petName: 'Goldie', petType: 'fish' },
 ]
 
+const initialFormValues = { petName: '', petType: '' }
+
 function SimpleForm() {
   const [pets, setPets] = useState(petsList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  console.log(formValues)
+
+  const change = (event) => {
+    const { name, value } = event.target
+    setFormValues({ ...formValues, [name]: value })
+    // const currentFormValues = {...formValues}
+    // const newObj = {}
+    // newObj[name] = value
+    // const final = {...currentFormValues, newObj}
+    // setFormValues(final)
+  }
+
   return (
     <div className='container'>
       <h1>Simple Form App</h1>
@@ -22,6 +37,20 @@ function SimpleForm() {
           </div>
         )
       })}
+      <form>
+        <input
+          type='text'
+          name='petName'
+          onChange={change}
+          value={formValues.petName}
+        />
+        <input 
+          type='text' 
+          name='petType' 
+          onChange={change} 
+          value={formValues.petType}
+        />
+      </form>
     </div>
   )
 }
