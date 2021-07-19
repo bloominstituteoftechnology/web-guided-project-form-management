999cfc9762cf668138a1cc115e4b9542aa13bedb
Rick <rmansfield@realpeoplesratings.com>
2021-07-19 16:58:11 -0500

    U2.3 M2 Gabe Lect Step 8 complete

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,7 @@ export default function App() {
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
     //  It takes in the name of an input and its value, and updates `formValues`
+    updateForm({...formValues, [inputName]: inputValue})
   }
 
   const submitForm = () => {
