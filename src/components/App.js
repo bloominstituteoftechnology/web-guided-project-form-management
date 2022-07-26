3c83b0144207e7e2a87a07da56c1b36b836144dd
Casey Harding <casey.r.harding@gmail.com>
2022-07-26 13:37:11 -0400

    Final solution in place

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,13 @@ export default function App() {
   const [friends, setFriends] = useState([]) // careful what you initialize your state to
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const [formValues, setFormValue] = useState(initialFormValues); // fix this using the state hook
+  const [formValues, setFormValues] = useState(initialFormValues); // fix this using the state hook
+  const [formError, setFormError] = useState("");
 
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
     //  It takes in the name of an input and its value, and updates `formValues`
+    setFormValues({ ...formValues, [inputName]: inputValue });
   }
 
   const submitForm = () => {
@@ -29,6 +31,22 @@ export default function App() {
     //  b) prevent further action if either username or email or role is empty string after trimming
     //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
     //  d) also on success clear the form
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if (!newFriend.username || !newFriend.email) {
+      setFormError("Gotta enter a username ya doofus!!");
+    } else {
+      axios.post("https://reqres.in/api/users", newFriend)
+        .then(res => {
+          setFriends([ ...friends, res.data ]);
+          setFormValues(initialFormValues);
+          setFormError("");
+        }).catch(err => console.error(err));
+    }
   }
 
   useEffect(() => {
@@ -38,7 +56,7 @@ export default function App() {
   return (
     <div className='container'>
       <h1>Form App</h1>
-
+      { formError && <h2 className="error">{formError}</h2> }
       <FriendForm
         // 🔥 STEP 2 - The form component needs its props.
         //  Check implementation of FriendForm
