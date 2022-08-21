9393ba0a3882bfcbc0885dcb2e6d18ebf8b28ba1
Valeriusdev <kakao757@gmail.com>
2022-08-20 21:49:55 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React, { useState } from "react";
+import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 First let's build a SimpleForm to add more pets:
 const petsList = [
-  { petName: 'Fido', petType: 'dog' },
-  { petName: 'Tweetie', petType: 'canary' },
-  { petName: 'Goldie', petType: 'fish' },
-]
+  { petName: "Fido", petType: "dog" },
+  { petName: "Tweetie", petType: "canary" },
+  { petName: "Goldie", petType: "fish" },
+];
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState({ petName: "", petType: "" });
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  return (
+    <div className="container">
+      <h1>Simple Form App</h1>
+      {pets.map((pet, idx) => {
+        return (
+          <div key={idx}>
+            {pet.petName}: {pet.petType}
+          </div>
+        );
+      })}
+      <form>
+        <input
+          value={formValues.petName}
+          name="petName"
+          type="text"
+          onChange={change}
+        />
+        <input
+          value={formValues.petType}
+          name="petType"
+          type="text"
+          onChange={change}
+        />
+      </form>
+    </div>
+  );
 }
 
 render(
   <>
     <SimpleForm />
     {/* <App /> */}
-  </>
-  , document.querySelector('#root')
-)
+  </>,
+  document.querySelector("#root")
+);
